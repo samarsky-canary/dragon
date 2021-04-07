cb1ab3f0630d112013c7fdb2130674e1c58cb54a
V.Samarsky <v.samarsky97@gmail.com>
2021-04-07 08:34:46 +0400

    dto

--- backend/src/curators/dto/create-curator.dto.ts
+++ backend/src/curators/dto/create-curator.dto.ts
@@ -1,12 +1,16 @@
 import {IsNotEmpty, IsString, IsUUID} from 'class-validator'
+import { User } from 'src/users/db/user.entity';
+import { ManyToOne } from 'typeorm';
 export class CuratorDto {
 
     @IsNotEmpty()
     @IsUUID()
+    @ManyToOne(type => User, user => user.id)
     uuid_curator: string;
 
     @IsNotEmpty()
     @IsUUID()
+    @ManyToOne(type => User, user => user.id)
     uuid_user: string;
 
     @IsString()
--- backend/src/users/dto/user.dto.ts
+++ backend/src/users/dto/user.dto.ts
@@ -1,7 +1,11 @@
 import {IsNotEmpty, IsString} from 'class-validator'
+import { Curator } from 'src/curators/db/curator.entity';
+import { OneToMany } from 'typeorm';
 export class UserDto {
 
     @IsString()
+    @OneToMany(type => Curator, curator => curator.uuid_user)
+    @OneToMany(type => Curator, curator => curator.uuid_curator)
     uuid: string;
 
 
