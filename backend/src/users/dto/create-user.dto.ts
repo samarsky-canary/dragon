1b5fb40201b5818f0f0e6494eb75a1bc0c413673
Vladislav Samarsky <v.samarsky97@gmail.com>
2021-06-19 17:52:39 +0400

    Fix login password DTO

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
-import {IsEmail, IsNotEmpty, IsString} from 'class-validator'
+import {IsEmail, IsNotEmpty, IsString, Matches} from 'class-validator'
 export class CreateUserDto {
 
     @IsNotEmpty()
     @IsString()
     username: string;
 
-    @IsNotEmpty()
     @IsString()
+    @IsNotEmpty()
+    @Matches(
+        /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/,
+        { message: 'Weak password' },
+    )
     password: string;
 
     @IsNotEmpty()
